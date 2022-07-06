3932ef8eece3c9a33cc004b799e3645f4f25c86b
Tremus <tredudman@yahoo.com.au>
2022-07-06 12:00:17 +1000

    added cool slider to control WT pos

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+# Plotting cool some shapes with Trémus
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+from matplotlib.widgets import Slider
 from typing import List
 
 N = 1 << 11
@@ -12,92 +15,109 @@ X_LABELS = [str(i) for i in X_TICKS]
 Y_TICKS = [-1, -0.5, 0, 0.5, 1]
 Y_LABELS = [str(i) for i in Y_TICKS]
 
-fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(7,2))
+x = np.arange(N)
 
 
-x = list(range(N))
+def create_sine(freq) -> np.ndarray:
+    # nth_harmonic = 1 + wt_pos * 15
+    p = (2 * np.pi * freq) / N
+    return np.sin(x * p)
 
+def create_saw(wt_pos) -> np.ndarray:
+    # return [-1 + (i / N * 2) for i in x]
+    return (x / N * 2) - 1
 
-def create_sine(nth_harmonic = 1) -> List[float]:
-    # RMS 0.7071067811865476
-    p = 1 / N * 2 * math.pi * nth_harmonic
-    return [math.sin(i * p) for i in x]
-
-def create_saw() -> List[float]:
-    # RMS 0.5773504068406402
-    return [-1 + (i / N * 2) for i in x]
-
-def create_fft_add_nths(n_stride = 1, num_harmonics = 4) -> List[float]:
+def create_fft_add_nths(wt_pos, num_harmonics = 4) -> List[float]:
     """sounds good when n_stride = 2/3/4"""
-    data = []
+    n_stride = round(1 + wt_pos * 3)
     harmonics = [1 + i * n_stride for i in range(num_harmonics)]
-    p = 1 / N * 2 * math.pi
-    for i in range(N):
-        v0 = i * p
-        v1 = 0
-        for idx, h in enumerate(harmonics):
-            v1 += math.sin(v0 * h)
-            # v1 += math.sin(v0 * h) / math.sqrt(2 + idx)
-        data.append(v1)
+    data = np.zeros(N)
+    for h in harmonics:
+        s = create_sine(h)
+        data = data + s
     return data
 
-def create_fft_add_nths_sqrt(n_stride = 1, num_harmonics = 4) -> List[float]:
-    data = []
+def create_fft_add_nths_sqrt(wt_pos, num_harmonics = 4) -> List[float]:
+    n_stride = round(1 + wt_pos * 7)
+    print(n_stride)
     harmonics = [1 + i * n_stride for i in range(num_harmonics)]
-    divisors = [1 / math.sqrt(1 + i) for i in range(num_harmonics)]
-    p = 1 / N * 2 * math.pi
-    for i in range(N):
-        v0 = i * p
-        v1 = 0
-        for idx, h in enumerate(harmonics):
-            v1 += math.sin(v0 * h) * divisors[idx]
-        data.append(v1)
+    amplitudes = [1 / math.sqrt(1 + i) for i in range(num_harmonics)]
+    data = np.zeros(N)
+    for h, amp in zip(harmonics, amplitudes):
+        s = create_sine(h) * amp
+        data = data + s
     return data
 
-def create_lp_saw(n_stride = 1, num_harmonics = 4) -> List[float]:
+def create_lp_saw(wt_pos, num_harmonics = 8) -> List[float]:
     """
     Square when n_stride = 2
     Anything >= 4 will start to look like a harmonic stretch on a Saw wave
+    Starts to get slow after 300 or so harmonics and an IFFT may be faster
     """
-    data = []
+    n_stride = round(1 + wt_pos * 7)
     harmonics = [1 + i * n_stride for i in range(num_harmonics)]
-    p = 1 / N * 2 * math.pi
-    for i in range(N):
-        v0 = i * p
-        v1 = 0
-        for h in harmonics:
-            # nth harmonic is divided by n
-            v1 += math.sin(v0 * h) / h
-        data.append(v1)
+    data = np.zeros(N)
+    for h in harmonics:
+        s = create_sine(h) / h
+        data = data + s
     return data
 
 
-# some serum WTs
-create_fft_add_2nds = lambda : create_fft_add_nths(2)
-create_fft_add_3rds = lambda : create_fft_add_nths(3)
-create_fft_add_4ths = lambda : create_fft_add_nths(4)
+# The parametrized function to be plotted
+def get_y(wt_pos):
+    y = create_lp_saw(wt_pos, 8)
+    # normalise to be between -1,1
+    y = y * (1 / max(np.max(y), abs(np.min(y))))
+    return y
 
-y = np.array(create_sine(1))
+def get_magnitudes(y, limit: int):
+    Y = np.fft.fft(y)
+    Y = Y[:limit]
+    # get magnitude, hypot of a complex number
+    magnitudes = np.hypot(np.real(Y), np.imag(Y))
+    return magnitudes
 
-# normalise the values to be between -1,1
-y = y * (1 / max(np.max(y), abs(np.min(y))))
+# Create the figure and the line that we will manipulate
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+y = get_y(0)
+line0, = ax0.plot(x, y)
+
+limit = 1 << 4
+bar_container = ax1.bar(x[:limit], get_magnitudes(y, limit))
+ax1.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=N, decimals=0))
 
 ax0.set_xticks(X_TICKS, X_LABELS)
 ax0.set_xlim(0, N)
 ax0.set_yticks(Y_TICKS, Y_LABELS)
 ax0.set_ylim(-1, 1)
-
-rms = np.sqrt(np.mean(np.array(y)**2))
-ax0.set_title("RMS: {:.3f}".format(rms))
-
-ax0.plot(x, y)
 ax0.grid()
 
-Y = np.fft.fft(y)
-crop = 1 << 4
-magnitudes = [math.hypot(c.real, c.imag) for c in Y[:crop]]
-magnitudes = np.array(magnitudes) / (sum(magnitudes) / 100)
-ax1.bar(x[:crop], magnitudes)
-ax1.yaxis.set_major_formatter(mtick.PercentFormatter())
+ax1.set_ylim(0, N)
+
+
+# adjust the main plot to make room for the sliders
+plt.subplots_adjust(bottom=0.25)
+
+# Make a horizontal slider to control the frequency.
+wt_slider = Slider(
+    ax=plt.axes([0.25, 0.1, 0.65, 0.03]),
+    label='Wavetable',
+    valmin=0,
+    valmax=1,
+    valinit=0)
+
+# The function to be called anytime a slider's value changes
+def update(val):
+    y = get_y(val)
+    line0.set_ydata(y)
+    mags = get_magnitudes(y, limit)
+    for rect, mag in zip(bar_container, mags):
+        rect.set_height(mag)
+    fig.canvas.draw_idle()
+
+
+# register the update function with each slider
+wt_slider.on_changed(update)
+
 
 plt.show()
